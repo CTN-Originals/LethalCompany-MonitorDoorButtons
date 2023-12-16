0bff970888c997b435940d2127395638e3d2b362
CTN <info@ctn-originals.com>
2023-12-16 15:46:07 +0100

    moved element path description
    It fits better above the functions that will apply the path descriptions to get the elements.

--- MonitorDoorButtons/Patches/StartOfRoundPatch.cs
+++ MonitorDoorButtons/Patches/StartOfRoundPatch.cs
@@ -22,10 +22,20 @@ namespace MonitorDoorButtons.Patches
 			GetDoorButtons();
 		}
 
+		//_ Monitor Camera buttons: Environment/HangarShip/ShipModels2b/MonitorWall/Cube.001/
+			//- CameraMonitorOnButton 
+				//> Cube (2)
+			//- CameraMonitorSwitchButton 
+				//> Cube (2)
 		private static void GetMonitorButton() {
 
 		}
 
+		//_ Door open/close panel: Environment/HangarShip/AnimatedShipDoor/HangarDoorButtonPanel/
+			//- StartButton
+				//> Cube (2)
+			//- StopButton 
+				//> Cube (3)
 		private static void GetDoorButtons() {
 
 		}

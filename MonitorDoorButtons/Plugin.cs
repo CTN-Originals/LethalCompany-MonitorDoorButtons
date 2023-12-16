c5b9a488da26d7f5b37c60d142ec5e426d4b7bcd
CTN <info@ctn-originals.com>
2023-12-16 14:59:34 +0100

    Update Plugin.cs
    Added better documentation for the objects I will need later.

--- MonitorDoorButtons/Plugin.cs
+++ MonitorDoorButtons/Plugin.cs
@@ -4,8 +4,16 @@ using HarmonyLib;
 
 
 
-//* Door open/close panel: Environment/HangarShip/AnimatedShipDoor/HangarDoorButtonPanel/ [StartButton, StopButton]
-//* Monitor Camera buttons: Environment/HangarShip/ShipModels2b/MonitorWall/Cube.001/ [CameraMonitorOnButton, CameraMonitorSwitchButton]
+//_ Door open/close panel: Environment/HangarShip/AnimatedShipDoor/HangarDoorButtonPanel/
+	//- StartButton
+		//> Cube (2)
+	//- StopButton 
+		//> Cube (3)
+//_ Monitor Camera buttons: Environment/HangarShip/ShipModels2b/MonitorWall/Cube.001/
+	//- CameraMonitorOnButton 
+		//> Cube (2)
+	//- CameraMonitorSwitchButton 
+		//> Cube (2)
 
 namespace MonitorDoorButtons
 {

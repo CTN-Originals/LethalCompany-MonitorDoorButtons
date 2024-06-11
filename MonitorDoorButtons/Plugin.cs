3e6fc848afc0c7facb51551877a2290f15d4c506
Korgen1 <joshkal9876@hotmail.com>
2024-06-11 02:05:43 -0400

    Added GI BetterMonitors compatability

--- MonitorDoorButtons/Plugin.cs
+++ MonitorDoorButtons/Plugin.cs
@@ -1,6 +1,10 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using BepInEx.Configuration;
+using JetBrains.Annotations;
+using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace MonitorDoorButtons
 {
@@ -10,13 +14,39 @@ namespace MonitorDoorButtons
 		private static readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
 
 		public static ManualLogSource CLog;
+		static bool betterMonitors;
+		static bool biggerButtons;
 
 		private void Awake() {
+
+			//Config
+			var configBetterMonitors = base.Config.Bind(
+				"General",
+				"ButtonsOnMonitor",
+				false,
+				"Moves the buttons onto the monitors above display controls and removes the back panel and power meter; required for GI bettermonitors compatability"
+			);
+			var configBiggerButtons = base.Config.Bind(
+				"General",
+				"BiggerButtons",
+				false,
+				"Makes the monitor buttons bigger, requires ButtonsOnMonitor to be true"
+			);
+
+			betterMonitors = configBetterMonitors.Value;
+			biggerButtons = configBiggerButtons.Value;
+
 			// Plugin startup logic
 			CLog = Logger;
 			CLog.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
 
 			harmony.PatchAll();
 		}
+		public static bool? GetConfig(int index){
+			if (index==1) { return betterMonitors; }
+			if (index==2) { return biggerButtons; }
+			return null;
+		}
+		
 	}
 }
